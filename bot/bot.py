a66dc03ee163f0c6f8b8e7edbb8c43c7412728c5
Sergei Shatrov <150809885+szarkans@users.noreply.github.com>
2026-04-09 11:52:12 +0300

    init

---
+++ bot/bot.py
@@ -0,0 +1,41 @@
+import asyncpg
+import disnake
+from disnake.ext import commands
+
+import os
+
+os.environ["PYTHONIOENCODING"] = "UTF-8"
+
+intents = disnake.Intents.all()
+
+class Bot(commands.Bot):
+    def __init__(self):
+        intents = disnake.Intents.all()
+        super().__init__(
+            intents=intents,
+            command_prefix='.к',
+            reload=True,
+            command_sync_flags=commands.CommandSyncFlags.all(),
+            strip_after_prefix=True,
+            test_guilds=[1138425078493753366],
+            owner_id=531208170098655233
+        )
+        self.pool: asyncpg.Pool = None
+
+    async def setup_hook(self):
+        self.pool = await asyncpg.create_pool(
+            dsn=os.getenv("DATABASE_URL"),
+            min_size=2,
+            max_size=10,
+        )
+
+    async def close(self):
+        if self.pool:
+            await self.pool.close()
+        await super().close()
+
+    @commands.event
+    async def on_ready(self):
+        print("Я родился!")
+
+bot = Bot()
---
+++ main.py
@@ -0,0 +1,40 @@
+from bot.bot import bot
+import os
+from dotenv import load_dotenv
+
+import logging
+from logging.handlers import RotatingFileHandler
+
+load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
+
+def setup_logger():
+    logger = logging.getLogger("robocat")
+    logger.setLevel(logging.DEBUG)
+
+    format = logging.Formatter(
+        "[%(asctime)s] [%(levelname)-8s] %(name)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+    file = RotatingFileHandler(
+        filename="logs/bot.log",
+        maxBytes=5 * 1024 * 1024,
+        backupCount=5,
+        encoding="UTF-8"
+    )
+    file.setLevel(logging.DEBUG)
+    file.setFormatter(format)
+
+    logger.addHandler(file)
+
+def load_extension():
+    extensions = [
+    ]
+    for i in extensions:
+        bot.load_extension(f"bot.{i}")
+
+
+setup_logger()
+load_extension()
+bot.run(token)
